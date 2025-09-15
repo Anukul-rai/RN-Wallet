8de49d19700518f264b394daea42a77294547e16
anukul rai <anukul.rau@gmail.com>
2025-09-15 20:14:52 +0545

    creating transcations route

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,62 @@ dotenv.config();
 
 const app = express();
 
+//Middleware(built-in)----between req and res
+app.use(express.json());
+
+//Custom simple middleware
+// app.use((req,res,next)=>{
+//     console.log("Hey we can hit a req, the method is",req.method,"and the url is",req.url);
+//     next();
+// })
+
 const PORT = process.env.PORT || 5001 ;
 
 async function initDB() {
     try{
-        await sql
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+        console.log("Database initialized successfully")
     }catch(err){
-        console.log("DB connection failed");
+        console.log("DB connection failed:",err);
+        process.exit(1); // status code 1 means failure,0 sucess
     }
 }
 
 app.get('/',(req,res)=>{
-    res.send("Hello Ashesh");
+    res.send("API is running....");
 })
-// console.log("my port :",process.env.PORT);
 
-app.listen(PORT,()=>{
-    console.log("Server is running on port:",PORT);
+app.post("/api/transactions",async (req,res)=>{
+    // title,amt,category.user_Id
+    try {
+        const {title,amount,category,user_id}=req.body
+
+        if(!title || amount === undefined || !category || !user_id){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        const transactions = await sql`
+        INSERT INTO transactions(title,amount,category,user_id)
+        VALUES(${title},${amount},${category},${user_id})
+        RETURNING *;
+        `
+        console.log(transactions)
+        res.status(201).json(transactions[0]);
+    } catch (error) {
+        console.log("Error in POST /api/transactions:",error);
+        res.status(500).json({message:"Internal server Error"});
+    }
 })
+
+
+initDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
