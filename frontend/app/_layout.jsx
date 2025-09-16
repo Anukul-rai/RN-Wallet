9306d79f137fdd32a89f08cea9aeda5dca07afcb
anukul rai <anukul.rau@gmail.com>
2025-09-16 23:19:00 +0545

    authentication of clerk

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,10 +1,17 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack screenOptions={{ headerShown: true }} />
-    </SafeScreen>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <SafeScreen>
+        <Slot />
+        {/* <Stack screenOptions={{ headerShown: true }} /> */}
+      </SafeScreen>
+    </ClerkProvider>
   );
 }
