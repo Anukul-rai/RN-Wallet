c43591f8d48ab8f183d2f1a221c5acb13a252d56
anukul rai <anukul.rau@gmail.com>
2025-09-16 23:49:12 +0545

    signup page UI completed

--- frontend/app/(auth)/sign-up.jsx
+++ frontend/app/(auth)/sign-up.jsx
@@ -1,10 +1,12 @@
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { useSignUp } from '@clerk/clerk-expo'
-import { Link, useRouter } from 'expo-router'
+import { useRouter } from 'expo-router'
 import { useState } from 'react'
 import { styles } from '@/assets/styles/auth.styles.js'
 import { Ionicons } from '@expo/vector-icons'
 import { COLORS } from '../../constants/colors'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 
 export default function SignUpScreen() {
   const { isLoaded, signUp, setActive } = useSignUp()
@@ -99,31 +101,58 @@ export default function SignUpScreen() {
   }
 
   return (
-    <View>
-      <>
-        <Text>Sign up</Text>
+    <KeyboardAwareScrollView 
+      style={{flex:1}}
+      contentContainerStyle={{flexGrow:1}}
+      enableOnAndroid={true}
+      enableAutomaticScroll={true}
+      extraScrollHeight={20}
+      >
+      <View style={styles.container}>
+        <Image source={require("../../assets/images/revenue-i4.png")} style={styles.illustration}/>
+
+        <Text style={styles.title}>Create an Acoount</Text>
+        {
+          error?(
+            <View style={styles.errorBox}>
+              <Ionicons name='alert-circle' size={20} color={COLORS.expense}/>
+              <Text style={styles.errorText}>{error}</Text>
+              <TouchableOpacity onPress={() => setError("")}>
+                <Ionicons name='close' size={20} color={COLORS.textLight}/>
+              </TouchableOpacity>
+            </View>
+          ):null
+        }
+
         <TextInput
+          style={[styles.input, error && styles.errorInput]}
           autoCapitalize="none"
           value={emailAddress}
           placeholder="Enter email"
+          placeholderTextColor={error ? COLORS.expense : COLORS.textLight}
           onChangeText={(email) => setEmailAddress(email)}
-        />
+          />
+
         <TextInput
+          style={[styles.input, error && styles.errorInput]}
           value={password}
           placeholder="Enter password"
-          secureTextEntry={true}
+          placeholderTextColor={error ? COLORS.expense : COLORS.textLight}
+          secureTextEntry
           onChangeText={(password) => setPassword(password)}
-        />
-        <TouchableOpacity onPress={onSignUpPress}>
-          <Text>Continue</Text>
+          />
+
+        <TouchableOpacity onPress={onSignUpPress} style={styles.button}>
+          <Text style={styles.buttonText}>Sign Up</Text>
         </TouchableOpacity>
-        <View style={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
-          <Text>Already have an account?</Text>
-          <Link href="/sign-in">
-            <Text>Sign in</Text>
-          </Link>
+
+        <View style={styles.footerContainer}>
+          <Text style={styles.footerText}>Already have an account?</Text>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Text style={styles.linkText}> Sign In</Text>
+          </TouchableOpacity>
         </View>
-      </>
-    </View>
+      </View>
+    </KeyboardAwareScrollView>
   )
 }
